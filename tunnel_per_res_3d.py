22235778a44631c239fb22b282ca7e15946d1a70
Tomek Wlodarski <tomek.wlodarski@gmail.com>
2024-04-18 23:53:06 +0200

    Update tunnel_per_res_3d.py

--- tunnel_per_res_3d.py
+++ tunnel_per_res_3d.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 from MDAnalysis.analysis import rms
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.interpolate import splprep, splev
-from kneed import KneeLocator
 import os
 
 #######################################################################################
